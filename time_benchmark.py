32f4c67558b7831d2ac47ff1d347df88d993cfee
Connor Lee <connorlee77@gmail.com>
2023-08-26 04:30:35 -0700

    Added torch compile to obtain 30 hz on titan rtx

--- time_benchmark.py
+++ time_benchmark.py
@@ -1,11 +1,17 @@
-#!/usr/bin/env python
-
 import argparse
-import time
 import torch
 import tqdm
 from models.models import Adaptive_Att_FusionNet
 from models.detector import DetBenchPredictImagePair
+
+def timed(fn):
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    start.record()
+    result = fn()
+    end.record()
+    torch.cuda.synchronize()
+    return result, start.elapsed_time(end) / 1000
 
 def run(args):
     # Benchmarking time: 
@@ -21,27 +27,26 @@ def run(args):
     bench = DetBenchPredictImagePair(model)
     bench = bench.cuda()
     bench.eval()
-
+    bench = torch.compile(bench, mode="reduce-overhead", backend="inductor")
+    
     model_config = bench.config
     param_count = sum([m.numel() for m in bench.parameters()])
     print('Model %s created, param count: %d' % (model_config.name, param_count))
+    
+    # Create dummy inputs
+    thermal_input = torch.randn(1, 3, 768, 768).cuda()
+    rgb_input = torch.randn(1, 3, 768, 768).cuda()
 
     # Warm up network
     with torch.no_grad():
-        for i in range(10):
-            thermal_input = torch.randn(1, 3, 768, 768).cuda()
-            rgb_input = torch.randn(1, 3, 768, 768).cuda()
+        for i in range(20):
             output = bench(thermal_input, rgb_input, img_info=None, branch=args.branch)
             
     total_time = 0
     with torch.no_grad():
-        for i in tqdm.tqdm(range(100), total=100):
-            thermal_input = torch.randn(1, 3, 768, 768).cuda()
-            rgb_input = torch.randn(1, 3, 768, 768).cuda()
-            start = time.time()
-            output = bench(thermal_input, rgb_input, img_info=None, branch=args.branch)
-            end = time.time()
-            total_time += end - start
+        for i in tqdm.tqdm(range(500), total=500):
+            _, t = timed(lambda: bench(thermal_input, rgb_input, img_info=None, branch=args.branch))
+            total_time += t
             average_time = total_time / (i + 1)
     print('Average time (forward pass): %.4f seconds' % average_time)    
 
