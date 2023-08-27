2f046766735fb90a8f33dd8f5bdf7fa45e5f93b7
Lu Gan <ganlu@umich.edu>
2023-08-27 03:05:25 -0700

    updated bash scripts

--- bash/val_agnostic_stf.sh
+++ bash/val_agnostic_stf.sh
@@ -1,4 +1,4 @@
 python validate_fusion.py Datasets/STF --dataset stf_full \
---checkpoint Checkpoints/STF/Fusion_Models/All_Trained/Full/model_best.pth \
+--checkpoint Checkpoints/STF/Fusion_Models/All_Trained/Full/model_best.pth.tar \
 --split test --num-classes 4 \
---model efficientdetv2_dt --batch-size=8 --branch fusion --att_type cbam
+--model efficientdetv2_dt --batch-size=8 --branch fusion --att_type cbam --classwise
